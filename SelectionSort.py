cfb7515c37ac3643be3cae1f928a490e7a81396b
krobus00 <michaelglaxy@gmail.com>
2021-01-03 13:39:14 +0700

    add images

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,13 +1,13 @@
 # Function recursive selection sort
 def selectionSortRecursive(A, i, n):
     min = i
-    for j in range(i + 1, n):
-        if A[j] < A[min]:
-            min = j
-    temp = A[min]
-    A[min] = A[i]
-    A[i] = temp
     if i + 1 < n:
+        for j in range(i + 1, n):
+            if A[j] < A[min]:
+                min = j
+        temp = A[min]
+        A[min] = A[i]
+        A[i] = temp    
         selectionSortRecursive(A, i + 1, n)
 
 # Function iterative selection sort
